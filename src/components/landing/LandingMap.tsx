4f405e39f19ec938a1b76b161d5685a3a4b33ac2
joshi-hahahaha <joshi.ha7@gmail.com>
2025-03-16 14:11:03 +1100

    FIX: search bar drop down.

--- src/components/landing/LandingMap.tsx
+++ src/components/landing/LandingMap.tsx
@@ -51,48 +51,83 @@ const LocationUpdater = ({
   ) : null;
 };
 
-const SearchArea = ({ setBounds }: { setBounds: (bounds: [number, number, number, number] | null) => void }) => {
+const SearchArea = ({
+  setBounds,
+}: {
+  setBounds: (bounds: [number, number, number, number] | null) => void;
+}) => {
   const map = useMap();
   return (
-    <button className="btn btn-primary text-primary-content absolute top-28 left-18 z-[1000]" onClick={() => {
-      const bounds = map.getBounds();
-      setBounds([
-        bounds.getSouth(),
-        bounds.getWest(),
-        bounds.getNorth(),
-        bounds.getEast()
-      ])
-    }}>
-      <svg fill="#000000" height="20" width="20" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.966 52.966" ><path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21 c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279 C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19	S32.459,40,21.983,40z" /></svg>
-      <p>Search This Area</p></button>
-  )
-}
+    <button
+      className="btn btn-primary text-primary-content absolute top-28 left-18 z-[500]"
+      onClick={() => {
+        const bounds = map.getBounds();
+        setBounds([
+          bounds.getSouth(),
+          bounds.getWest(),
+          bounds.getNorth(),
+          bounds.getEast(),
+        ]);
+      }}
+    >
+      <svg
+        fill="#000000"
+        height="20"
+        width="20"
+        version="1.1"
+        id="Capa_1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 52.966 52.966"
+      >
+        <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21 c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279 C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19	S32.459,40,21.983,40z" />
+      </svg>
+      <p>Search This Area</p>
+    </button>
+  );
+};
 
 const LandingMap = () => {
-  const defaultCenter = useMemo(() => [-33.8708, 151.2073] as [number, number], []);
-  const [userLocation, setUserLocation] = useState<[number, number]>(defaultCenter);
-  const [zoom,] = useState(11);
+  const defaultCenter = useMemo(
+    () => [-33.8708, 151.2073] as [number, number],
+    []
+  );
+  const [userLocation, setUserLocation] =
+    useState<[number, number]>(defaultCenter);
+  const [zoom] = useState(11);
   const [selectedToilet, setSelectedToilet] = useState<ToiletType | null>(null);
   const [routeFinish, setRouteFinish] = useState<[number, number] | null>(null);
   const [showToilets, setShowToilets] = useState<boolean>(true);
-  const [bounds, setBounds] = useState<[number, number, number, number] | null>(null);
+  const [bounds, setBounds] = useState<[number, number, number, number] | null>(
+    null
+  );
 
   useEffect(() => {
     if (!!navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setBounds([latitude - 0.1, longitude - 0.15, latitude + 0.1, longitude + 0.15])
-      },
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setBounds([
+            latitude - 0.1,
+            longitude - 0.15,
+            latitude + 0.1,
+            longitude + 0.15,
+          ]);
+        },
         (error) => {
           console.error("Error getting location:", error);
-          setBounds([defaultCenter[0] - 0.05, defaultCenter[1] - 0.05, defaultCenter[0] + 0.05, defaultCenter[1] + 0.05]);
+          setBounds([
+            defaultCenter[0] - 0.05,
+            defaultCenter[1] - 0.05,
+            defaultCenter[0] + 0.05,
+            defaultCenter[1] + 0.05,
+          ]);
         }
       );
     }
   }, [defaultCenter]);
 
   useEffect(() => {
-    setShowToilets(!routeFinish) // hide toilets when routing
+    setShowToilets(!routeFinish); // hide toilets when routing
   }, [routeFinish]);
 
   useEffect(() => {
@@ -118,11 +153,17 @@ const LandingMap = () => {
           </Marker>
         )}
 
-        {showToilets && !!bounds && <PublicToilets bbox={bounds} setSelectedToilet={setSelectedToilet} />}
+        {showToilets && !!bounds && (
+          <PublicToilets bbox={bounds} setSelectedToilet={setSelectedToilet} />
+        )}
         <SearchArea setBounds={setBounds} />
         <RoutingMachine start={userLocation} finish={routeFinish} />
       </MapContainer>
-      <ToiletInfo toilet={selectedToilet} setSelectedToilet={setSelectedToilet} setRouteFinish={setRouteFinish} />
+      <ToiletInfo
+        toilet={selectedToilet}
+        setSelectedToilet={setSelectedToilet}
+        setRouteFinish={setRouteFinish}
+      />
     </div>
   );
 };
--- src/components/landing/LandingNav.tsx
+++ src/components/landing/LandingNav.tsx
@@ -7,13 +7,17 @@ import { useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-control-geocoder";
 
-function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number, number] | null) => void }) {
+function LandingNav({
+  setBounds,
+}: {
+  setBounds: (bounds: [number, number, number, number] | null) => void;
+}) {
   const router = useRouter();
   const map = useMap();
 
   const [searchBarValue, setSearchBarValue] = useState("");
   const [signedIn, setSignedIn] = useState<boolean>(false);
-  const [searchDropDownValues, setSearchDropDownValues] = useState([])
+  const [searchDropDownValues, setSearchDropDownValues] = useState([]);
 
   const tempToggle = () => {
     setSignedIn(!signedIn);
@@ -22,7 +26,7 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
   const handleSearch = async () => {
     if (!searchBarValue || searchBarValue === "") return;
     const geocoder = L.Control.Geocoder.nominatim();
-    console.log("going to try to geocode: " + geocoder.geocode)
+    console.log("going to try to geocode: " + geocoder.geocode);
 
     const res = await geocoder.geocode(searchBarValue);
     console.log(res);
@@ -34,7 +38,7 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
       location.bbox.getSouthEast(),
       location.bbox.getNorthEast(),
       location.bbox.getNorthWest(),
-      location.bbox.getSouthWest()
+      location.bbox.getSouthWest(),
     ]).addTo(map);
     map.fitBounds(poly.getBounds());
 
@@ -42,7 +46,7 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
       location.bbox.getSouth(),
       location.bbox.getWest(),
       location.bbox.getNorth(),
-      location.bbox.getEast()
+      location.bbox.getEast(),
     ]);
 
     setTimeout(() => {
@@ -50,7 +54,7 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
     }, 3000);
 
     setSearchDropDownValues([]);
-  }
+  };
 
   return (
     <div className="absolute top-4 left-4 right-4 bg-base-100 text-black h-16 flex items-center justify-between px-6 rounded-xl shadow-md z-[1000]">
@@ -62,7 +66,6 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
       {/* Center: Search Bar (Geocoder Integrated) */}
       <div className="join">
         <input
-
           popoverTarget="popover-1"
           className="input join-item w-full"
           placeholder="Search"
@@ -76,10 +79,17 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
 
       {/* Dropdown appears only when there are values */}
       {searchDropDownValues.length > 0 && (
-        <ul className="menu dropdown-content absolute left-0 w-full shadow-lg border mt-1 z-10" id="popover-1">
+        <ul
+          className="absolute left-0 w-full shadow-lg border mt-1 z-100 bg-white rounded-md overflow-hidden max-h-60 overflow-y-auto "
+          id="popover-1"
+          style={{ top: "100%", position: "absolute" }}
+        >
           {searchDropDownValues.map((x, i) => (
-            <li key={i}>
-              <button className="btn w-full text-left" onClick={() => handleDropDownSelect(x)}>
+            <li key={i} className="border-b last:border-none">
+              <button
+                className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                onClick={() => handleDropDownSelect(x)}
+              >
                 {x.name}
               </button>
             </li>
@@ -115,10 +125,18 @@ function LandingNav({ setBounds }: { setBounds: (bounds: [number, number, number
         <label className="swap swap-rotate">
           <input type="checkbox" className="theme-controller" value="dark" />
           {/* Icons for theme switching */}
-          <svg className="swap-off h-6 w-6 fill-accent-content" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <svg
+            className="swap-off h-6 w-6 fill-accent-content"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
             <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
-          <svg className="swap-on h-6 w-6 fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <svg
+            className="swap-on h-6 w-6 fill-primary"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
             <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
